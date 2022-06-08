aba25c5607460f306fe4c6803d5c90ac572f8529
Daniel Alves <dani.rochapq@gmail.com>
2022-06-08 18:56:55 -0300

    estudo sobre arraylist (testes)

--- mc322/trabalho/src/AppLost.java
+++ mc322/trabalho/src/AppLost.java
@@ -1,15 +1,37 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AppLost {
 
 	public static void main(String[] args) {
-		char comando;
-		ControleJogo cj = new ControleJogo();
-		Scanner in = new Scanner(System.in);
-		System.out.println("Hello world!");
-		comando = in.nextLine().charAt(0);
+		ArrayList<String> cars = new ArrayList<String>();
+	    cars.add("Volvo");
+	    cars.add("BMW");
+	    cars.add("Ford");
+	    cars.add("Mazda");
+	    
+	    //varifica se algo especifico esta no arraylist
+	    if(cars.contains("Mazda")) {
+	    	System.out.println("tem sim");
+	    }
 		
-		cj.comando(comando);
+	    System.out.println(cars);
+	    
+	    //percorre arraylist e pega o elemento do indice i
+		for (int i = 0; i < cars.size(); i++) {
+			System.out.println(cars.get(i));
+		}
+		
+		//remove elemento da posicao 0 do arraylist
+		cars.remove(0);
+		
+		//remove o elemento dado como parametro
+		cars.remove("BMW");
+		
+		System.out.println(cars);
+		
+		for (int i = 0; i < cars.size(); i++) {
+			System.out.println(cars.get(i));
+		}
 	}
-
 }
