a231d5b426c271ded0e1d912c4da9dc4d7f23f9a
Daniel Alves <dani.rochapq@gmail.com>
2022-06-08 13:12:49 -0300

    adiciona classe ControleJogo

--- mc322/trabalho/src/AppLost.java
+++ mc322/trabalho/src/AppLost.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
 
 public class AppLost {
 
 	public static void main(String[] args) {
+		char comando;
+		ControleJogo cj = new ControleJogo();
+		Scanner in = new Scanner(System.in);
 		System.out.println("Hello world!");
+		comando = in.nextLine().charAt(0);
+		
+		cj.comando(comando);
 	}
 
 }
---
+++ mc322/trabalho/src/ControleJogo.java
@@ -0,0 +1,9 @@
+
+public class ControleJogo {
+	private char movimento;
+	private char acao;
+	
+	public void comando(char c) {
+		System.out.println("o comando foi recebido foi: " + c);
+	}
+}
