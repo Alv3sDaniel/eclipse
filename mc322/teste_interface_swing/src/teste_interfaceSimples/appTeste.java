b9eb8807715077ae10506a665f131a683d604634
Daniel Alves <102082350+Alv3sDaniel@users.noreply.github.com>
2022-06-23 18:36:14 -0300

    Add files via upload

---
+++ mc322/teste_interface_swing/src/teste_interfaceSimples/appTeste.java
@@ -0,0 +1,14 @@
+package teste_interfaceSimples;
+
+import javax.swing.JFrame;
+
+public class appTeste {
+
+	public static void main(String[] args) {
+		JFrame janela =new JFrame();
+		janela.setSize(300,200);
+		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		janela.setVisible(true);
+	}
+
+}
